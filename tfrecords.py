c5a0d2d333bbb864ccce3b39267915144d514234
liuxcc <liuxcc@163.com>
2018-08-26 21:13:32 +0800

    delete some comments

--- tfrecords.py
+++ tfrecords.py
@@ -116,31 +116,3 @@ if __name__ == '__main__':
     tf_file = './data/LFW_MTCNN.tfrecords'
     stored_to_tfrecords(txt_file, tf_file, 'LFW')
 
-
-    # for serialized_example in tf.python_io.tf_record_iterator("./data/train.tfrecords"):
-    #     example = tf.train.Example()
-    #     example.ParseFromString(serialized_example)
-
-    #     image = example.features.feature['image'].bytes_list.value
-    #     label = example.features.feature['label'].int64_list.value
-    #     # 可以做一些预处理之类的
-    #     print(image, label)
-
-    # img, label = read_and_decode("./data/train.tfrecords")
-
-    # #使用shuffle_batch可以随机打乱输入
-    # img_batch, label_batch = tf.train.shuffle_batch([img, label],
-    #                                                 batch_size=64, capacity=2000,
-    #                                                 min_after_dequeue=1000)
-    # init = tf.global_variables_initializer()
-    # # coord = tf.train.Coordinator()
-    # with tf.Session() as sess:
-    #     sess.run(init)
-    #     threads = tf.train.start_queue_runners(sess=sess)
-    #     for i in range(3):
-    #         val, l= sess.run([img_batch, label_batch])
-    #         #我们也可以根据需要对val， l进行处理
-    #         #l = to_categorical(l, 12) 
-    #         print(val.shape, l)
-    #     # coord.request_stop()
-    #     # coord.join(threads)
